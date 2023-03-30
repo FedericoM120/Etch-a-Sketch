57b96e4cf2bcd805f5e74d48388ef47fccd12040
Federico Monteverdi <monteverdi.102@gmail.com>
2023-03-29 20:17:32 -0500

    edited js, html and css file

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const mainClass = document.querySelector('.mainClass');
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++){
         const div = document.createElement("div");
+        div.classList.add("block");
+        div.style.borderColor = "black";
+        const textNode = document.createTextNode("hi");
+        div.appendChild(textNode);
         mainClass.appendChild(div);
-
     }    
 }
-
-
-div.style.border = "black"
