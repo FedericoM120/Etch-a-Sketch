3f39fab9de9e37fc8f12d7b836c1cf8615222d8e
Federico Monteverdi <monteverdi.102@gmail.com>
2023-04-03 19:32:31 -0500

    updates

--- index.js
+++ index.js
@@ -7,9 +7,22 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++){
         const div = document.createElement("div");
         div.classList.add("block");
+        div.style.border = "medium solid black"
         div.style.borderColor = "black";
-        const textNode = document.createTextNode("hi");
+        const textNode = document.createTextNode("\u00A0");
         div.appendChild(textNode);
         mainClass.appendChild(div);
+        
     }    
 }
+
+const blocks = document.querySelectorAll('.block');
+for (let i = 0; i < blocks.length; i++) {
+    blocks[i].addEventListener("mouseover", function(e) {
+        e.target.classList.add("colorClass");
+    })
+}
+/*ument.getElementsByClassName("block").addEventListener("mouseover", function(e) {
+    div.classList.add("colorClass");
+} )*/
+
